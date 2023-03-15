06003f084fe6df2834ccae9785ab10d68024bd47
Monique Lupe <moniquelupeandrade@gmail.com>
2023-03-15 11:34:23 -0300

    Criei o filtro de diretores

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
-console.log(data.studio);
-console.log(data.films[0].title);
-for (let index = 0; index < 20; index++) { // a crase inicia a template string e também finaliza
-  let cartao = `
+// Declarar a função printar cartões:
+const printarCartoes = function () {
+  document.getElementById("cartões").innerHTML = ""
+  for (let index = 0; index < 20; index++) { // a crase inicia a template string e também finaliza
+    let cartao = `
     <div class="card">
       <h2>${data.films[index].title}</h2>
       <p>Informações sobre o filme:${data.films[index].description}</p>
       <img class="image" src="${data.films[index].poster}" alt="Poster do filme ${data.films[index].title}"/>
     </div>
   `;
-  console.log(cartao);
-  document.getElementById("cartões").innerHTML += cartao; // inclui no html 
+    document.getElementById("cartões").innerHTML += cartao; // inclui no html 
+  }
 }
+// Executar a função:
+printarCartoes()
+const diretores = document.getElementById("diretores")
+diretores.addEventListener("change", function (event) {
+  filtrarCartoes(event.target.value)
+})
 
-
-// outra forma de fazer -> data.filmes.forEach
-// template string ->`<div> ${data.films[index].title}` substitui -> "<div>"+ variavel + "<div>"
+// Declarar função filtrar cartões:
+const filtrarCartoes = function (diretorSelecionado) {
+  const arrayDeFilmes = data.films
+  const diretorFiltrado = [];
+  for (let i of data.films) {
+    if (i.director.toLocaleLowerCase() === diretorSelecionado.toLocaleLowerCase()) {
+      diretorFiltrado.push(i);
+    }
+  }
+}
