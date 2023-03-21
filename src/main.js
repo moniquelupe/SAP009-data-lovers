a93ad72e744d719c971f98057022038d7ba73304
Monique Lupe <moniquelupeandrade@gmail.com>
2023-03-21 13:57:52 -0300

    Erro CSS e flexbox

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
 import data from './data/ghibli/ghibli.js';
 
 // Declarar a função printar cartões:
-const printarCartoes = function () {
+const printarCartoes = function (arrayTela) {
   document.getElementById("cartões").innerHTML = ""
-  for (let index = 0; index < 20; index++) { // a crase inicia a template string e também finaliza
+  for (let index = 0; index < arrayTela.length; index++) { // a crase inicia a template string e também finaliza
     let cartao = `
     <div class="card">
-      <img class="image" src="${data.films[index].poster}" alt="Poster do filme ${data.films[index].title}"/>
-      <h2>${data.films[index].title}</h2>
-      <p>${data.films[index].description}</p>
+      <img class="image" src="${arrayTela[index].poster}" alt="Poster do filme ${arrayTela[index].title}"/>
+      <h2>${arrayTela[index].title}</h2>
+      <p>${arrayTela[index].description}</p>
     </div>
   `;
     document.getElementById("cartões").innerHTML += cartao; // inclui no html 
   }
 }
 // Executar a função:
-printarCartoes()
+printarCartoes(data.films)
 const diretores = document.getElementById("diretores")
 diretores.addEventListener("change", function (event) {
   filtrarCartoes(event.target.value)
 })
-
 
 // Declarar função filtrar cartões:
 const filtrarCartoes = function (diretorSelecionado) {
@@ -31,15 +30,21 @@ const filtrarCartoes = function (diretorSelecionado) {
       diretorFiltrado.push(i);
     }
   }
-  let cartoesFiltrados = "";
-  for (let i = 0; i < diretorFiltrado.length; i++) {
-    cartoesFiltrados += `
-    <div class="card">
-      <img class="image" src="${diretorFiltrado[i].poster}" alt="Poster do filme ${diretorFiltrado[i].title}"/>
-      <h2>${diretorFiltrado[i].title}</h2>
-      <p>${diretorFiltrado[i].description}</p>
-    </div>
-    `;
-  }
-  document.getElementById("cartões").innerHTML = cartoesFiltrados;
+  printarCartoes(diretorFiltrado)
 }
+
+const timeline = document.getElementById("timeline-id")
+timeline.addEventListener("change", function (event){
+  ordenarTempo(event.target.value)
+})
+
+// const ordenarTempo = function (ordemSelecionada) {
+  // próximo passo: usar método sort pra criar essa função que vai entrar aqui
+ // const arrayDeFilmes = data.films;
+ // if (ordemSelecionada = "Antigo para recente") {
+ //   arrayDeFilmes.sort(a,b)
+    
+ // }
+   
+// }
+
